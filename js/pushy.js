9378e5327f470ab4cde33c77db981d3455e67688
Maia McGuinness <maiamcguinness@gmail.com>
2013-11-14 15:43:11 -0500

    Updated forked Pushy version to my own. Storing for future use. Will update README with my own info shortly.

--- js/pushy.js
+++ js/pushy.js
@@ -1,44 +1,68 @@
-/*! Pushy - v0.9.1 - 2013-9-16
+/*! Pushy - v0.9 - 2013-6-22
 * Pushy is a responsive off-canvas navigation menu using CSS transforms & transitions.
 * https://github.com/christophery/pushy/
 * by Christopher Yee */
 
-$(function() {
+jQuery(function($) {
 	var pushy = $('.pushy'), //menu css class
 		body = $('body'),
 		container = $('#container'), //container css class
-		push = $('.push'), //css class to add pushy capability
 		siteOverlay = $('.site-overlay'), //site overlay
-		pushyClass = "pushy-left pushy-open", //menu position & menu open class
+		pushyClass = "pushy-open", //menu position & menu open class
 		pushyActiveClass = "pushy-active", //css class to toggle site overlay
 		containerClass = "container-push", //container open class
-		pushClass = "push-push", //css class to add pushy capability
-		menuBtn = $('.menu-btn, .pushy a'), //css classes to toggle the menu
-		menuSpeed = 200, //jQuery fallback menu speed
-		menuWidth = pushy.width() + "px"; //jQuery fallback menu width
+		menuBtn = $('.menu-btn'), //css classes to toggle the menu
+		menuSpeed = 100, //jQuery fallback menu speed
+		menuWidth = "21.5em"; //jQuery fallback menu width
 
 	function togglePushy(){
 		body.toggleClass(pushyActiveClass); //toggle site overlay
 		pushy.toggleClass(pushyClass);
 		container.toggleClass(containerClass);
-		push.toggleClass(pushClass); //css class to add pushy capability
+		widthFix();
 	}
 
 	function openPushyFallback(){
 		body.addClass(pushyActiveClass);
-		pushy.animate({left: "0px"}, menuSpeed);
+		container.css({"position": "fixed"});
+		pushy.animate({left:"0"}, menuSpeed);
 		container.animate({left: menuWidth}, menuSpeed);
-		push.animate({left: menuWidth}, menuSpeed); //css class to add pushy capability
+		widthFix();
 	}
 
 	function closePushyFallback(){
 		body.removeClass(pushyActiveClass);
+		container.css({"position": "relative"});
 		pushy.animate({left: "-" + menuWidth}, menuSpeed);
 		container.animate({left: "0px"}, menuSpeed);
-		push.animate({left: "0px"}, menuSpeed); //css class to add pushy capability
 	}
 
-	if(Modernizr.csstransforms3d){
+	function widthFix() {
+		var pageWidth;
+		setPageWidth();
+		
+		function setPageWidth(){
+			pageWidth = getWidth() + "px";
+			
+			var w = getWidth();
+			if (w < 768) { // fixes some screwy behavior above 768px
+				container.css({"width": pageWidth});
+			} else {
+				container.css({"width": "auto"});
+			}
+		}
+		
+		if (!window.addEventListener) {
+			//IE
+			$( window ).resize(setPageWidth);
+			
+		} else {
+			//modern browsers
+			window.addEventListener('resize', setPageWidth);
+		}
+	}
+	
+	if($( "html" ).hasClass( "csstransforms" )){
 		//toggle menu
 		menuBtn.click(function() {
 			togglePushy();
@@ -76,5 +100,41 @@ $(function() {
 				state = true;
 			}
 		});
+	}
+	
+	function closePushyOnResize(){
+		var w = getWidth(); //even though page width defined above, forcing it to recalculate
+		
+		//close Pushy when the window gets too big
+		if (w >= 768 && $( ".pushy" ).hasClass( "pushy-open" )) {
+			togglePushy();
+		}
+	}
+	
+	if (!window.addEventListener) {
+		//IE
+		$( window ).resize(closePushyOnResize);
+		
+	} else {
+		//modern browsers
+		window.addEventListener('resize', closePushyOnResize);
+	}
+	
+	function getWidth() {
+		var w = 0;
+		//IE
+		if(!window.innerWidth){
+			if(!(document.documentElement.clientWidth == 0)){
+			//strict mode
+				w = document.documentElement.clientWidth;
+			} else{
+			//quirks mode
+				w = document.body.clientWidth;
+			}
+		} else {
+			//w3c
+			w = window.innerWidth;
+		}
+		return w;
 	}
 });
